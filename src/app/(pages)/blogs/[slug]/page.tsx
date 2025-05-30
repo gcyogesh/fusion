3c0c75e99eacd100e066a3a111e559b169f7c63b
kapil <kapilkharal.076@kathford.edu.np>
2025-05-30 14:39:46 +0545

    contact

--- src/app/(pages)/blogs/[slug]/page.tsx
+++ src/app/(pages)/blogs/[slug]/page.tsx
@@ -16,6 +16,20 @@ const socialLinks = [
   { icon: <FaFacebookF className="text-white bg-[#F7941D] p-2 rounded-xl w-8 h-8"/>, link: "#" },
   { icon: <FaYoutube className="text-white bg-[#F7941D] p-2 rounded-xl w-8 h-8"/>, link: "#" },
 ];
+
+const intro =
+  "Nepal is a country of contrasts with spectacular natural richness, combine with a vibrant cultural heritage, spiritual sites and a rich historical legacy. Home to 8 of the world’s 14 highest mountains, makes this country an unparalleled destination for trekkers and mountaineers alike.";
+
+const heading = "Here are some compelling reasons to explore this Himalayan nation:";
+
+const points = [
+  "Paradise for Adventure Seeker: Nepal is often referred to as a playground for adventure enthusiasts due to the wide array of thrilling activities and experiences it offers. From scaling the world’s highest peaks to navigating challenging rapids, it provides countless opportunities for adventure seekers to push their limits and embrace the adrenaline rush. Whether you seek thrilling mountaineering expeditions, awe-inspiring trekking adventures, or some adrenaline pumping activities like Paragliding, white water rafting, bunjee jumping, zip-lining, mountain biking and rock climbing, Nepal has no dearth of adventure options.",
+  "Cultural and Spiritual Heritage: One of the most prominent aspects of Nepali culture is its religion. The majority of the population practices Hinduism, while Buddhism also holds significance presence. The ancient temples, shrines, and monasteries scattered throughout the country showcase the deep spiritual beliefs and rituals that have been followed for centuries. There are more than 125 different ethnic groups in Nepal, following different religions from Hinduism, Tibetan Buddhism to shamanistic and animistic practice of Bon, which made the land culturally rich. The birthplace of Gautam Buddha, Nepal is a spiritual hub for seekers and spiritual practitioners. It is a home to numerous monasteries, gompas (Buddhist monastic complexes), and retreat centers where people can engage in meditation, reflection, mindfulness and seek spiritual guidance to self-discovery and enlightenment.",
+  "Rich in Wildlife and Natural Biodiversity: Nepal is home to the endangered Bengal Tiger, big Himalayan cat: Snow Leopard, One-horned Rhino, Asian Elephant, Red Panda and over 900 recorded bird species. Because of its huge geographical variation, ranging from lowland of Terai to high Himalayan mountains, Nepal boasts of verdant forests which provides shelter to 30,167 living species; including 13,067 plants and 17,097 animals. Around 23% of country’s land is covered by various protected areas, including national parks, wildlife reserves, and conservation areas with notable sites like Chitwan National Park, Sagarmatha National Park and Bardia National Park. The country’s ecosystems include wetlands, river systems, forests and alpine habitats which help maintain country’s ecological balance and preserve its natural heritage.",
+  "Warm Hospitality of the friendliest People: People in Nepal are genuinely warm and welcoming. The concept of “Atithi Devo Bhava” (Guest is God) is deeply ingrained in the Nepalese psyche, emphasizing the importance of welcoming and taking care of the visitors. People in Nepalese society are raised with a sense of collective responsibility and taking care of one another, which is why guests are treated as part of the extended family, fostering a welcoming and inclusive environment. Offering tea or food is a very common practice in Nepalese household when guests arrive, which shows care and hospitality, allowing guests to feel at home.  Nepalese people genuinely value human connections and believe in treating others with kindness and compassion, irrespective of social or cultural differences, that’s why there comes a saying that “While you first come to Nepal for the mountains, you return here for the people.",
+  "Great Value For Your Money: And finally comes a matter of Money! Nepal was named the world’s ‘best value destination’ by Lonely Planet in 2017, because it’s easy to explore this beautiful, culturally rich country on a budget. Nepalese cuisine, from Dal Bhat (rice with lentil soup and curry) to Momos (vegetables or meat dumplings), is known for its delicacy and affordability, although some fancy restaurants in Kathmandu might be expensive as costs are creeping up in the Kathmandu Valley like every other Metropolis. Public transportation is generally good and inexpensive. According to individual’s preferences, there are plenty of options for accommodations from budget Hotels/ hostels to luxuries star Hotels and entry fees for the heritage sites are also reasonable. If you’re a budget traveler and can adjust with basic facilities, $30-50 a day will be enough for you to explore Nepal comfortably.",
+];
+
 export default async function Page({ params }: PageProps) {
   const blogEndpoint = `blogs/${params.slug}`;
   const blogdata = await fetchAPI({ endpoint: blogEndpoint });
@@ -38,7 +52,7 @@ export default async function Page({ params }: PageProps) {
           <TextDescription text={blogdata.data.description}  className="text-[#535556]"/>
 
           {/* Quick Facts Section */}
-          <div className="border border-gray-300 rounded-md p-6 bg-white shadow-sm">
+          <div className="border border-gray-300 rounded-md p-6 bg-white shadow-sm mb-4">
             <TextHeader text="Quick Facts about Nepal" align="left" size="large" width={855} className=" w-[815px] h-auto mb-2" />
             
             <ul className="list-disc list-inside text-base text-[#535556] space-y-2 pl-2">
@@ -54,9 +68,19 @@ export default async function Page({ params }: PageProps) {
               <li>Driving: on the left-hand side</li>
               <li>Area: 147,516 sq. km</li>
             </ul>
-          </div>
 
+
+          </div>
           
+      <TextHeader text="Why Visit Nepal?" align="left" size="large" width={855} className=" w-[815px] h-auto" />
+      <TextDescription text={intro} className="font-semibold text-[#535556]" />
+      <TextDescription text={heading} className="font-semibold text-[#535556]" />
+      {points.map((point, index) => (
+        <div key={index} className="flex items-start gap-4">
+          <div className="text-sm font-semibold text-[#535556] mt-2.5">{index + 1}.</div>
+          <TextDescription text={point} className="text-justify text-[#535556]" />
+        </div>
+      ))}
         </div>
 
         {/* Sidebar */}
